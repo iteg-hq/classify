12889f84c5adc739666af9155c166dc95395f2a9
Frederik Siegumfeldt <fsi@iteg.dk>
2019-08-14 13:36:44 +0200

    Added setup schema for system data (countries, boolean flag, currencies)

---
+++ Classify/Setup/Currency.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE setup.Currency
+AS
+EXEC SaveClassifierType 'Currency', 'Currency', 'An ISO 4217 currency';
+EXEC SaveClassifier 'Currency', 'DKK',  'Danish Kroner';
+EXEC SaveClassifier 'Currency', 'GBP',  'British Pound';
+EXEC SaveClassifier 'Currency', 'EUR',  'Euro';
+
+EXEC SaveClassifierType 'CurrencyPair', 'Currency Pair', 'An ISO 4217 currency pair';
+EXEC SaveClassifier 'CurrencyPair', 'GBP/DKK',  'British Pound in Kroner';
+EXEC SaveClassifier 'CurrencyPair', 'EUR/DKK',  'Euro in Kroner';
+
+EXEC SaveClassifierRelationship 'CurrencyPair', 'EUR/DKK', 'ExchangeRate', 'Date', '2019-01-01', @Weight = 746.33;
+EXEC SaveClassifierRelationship 'CurrencyPair', 'GBP/DKK', 'ExchangeRate', 'Date', '2019-01-01', @Weight = 803.98;
---
+++ Classify/Setup/Flag.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE setup.Flag
+AS
+EXEC SaveClassifierType 'Flag', 'Flag', 'Logical values (true/false).';
+EXEC SaveClassifier 'Flag', '1',  'True';
+EXEC SaveClassifier 'Flag', '0', 'False';
