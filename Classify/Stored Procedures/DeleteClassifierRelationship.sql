084907a8796b3c541aa5de4e6bf1c344d0c69bb8
Frederik Siegumfeldt <fsi@iteg.dk>
2019-08-14 13:39:39 +0200

    Added SP to delete relationships

---
+++ Classify/Stored Procedures/DeleteClassifierRelationship.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE dbo.DeleteClassifierRelationship
+    @ClassifierTypeCode NVARCHAR(200)
+  , @ClassifierCode NVARCHAR(200)
+  , @ClassifierRelationshipType NVARCHAR(200)
+  , @RelatedClassifierTypeCode NVARCHAR(200)
+  , @RelatedClassifierCode NVARCHAR(200)
+AS
+SET NOCOUNT, XACT_ABORT ON;
+
+DECLARE @ClassifierTypeCodeID INT;
+DECLARE @ClassifierCodeID INT;
+DECLARE @ClassifierRelationshipTypeID INT;
+DECLARE @RelatedClassifierTypeCodeID INT;
+DECLARE @RelatedClassifierCodeID INT;
+
+EXEC internal.GetCodeID @ClassifierTypeCode,         @ClassifierTypeCodeID OUTPUT;
+EXEC internal.GetCodeID @ClassifierCode,             @ClassifierCodeID OUTPUT;
+EXEC internal.GetCodeID @ClassifierRelationshipType, @ClassifierRelationshipTypeID OUTPUT;
+EXEC internal.GetCodeID @RelatedClassifierTypeCode,  @RelatedClassifierTypeCodeID OUTPUT;
+EXEC internal.GetCodeID @RelatedClassifierCode,      @RelatedClassifierCodeID OUTPUT;
+
+INSERT INTO internal.ClassifierRelationship (
+    ClassifierTypeCodeID
+  , ClassifierCodeID
+  , RelationshipTypeCodeID
+  , RelatedClassifierTypeCodeID
+  , RelatedClassifierCodeID
+  , IsDeleted
+  )
+VALUES (
+    @ClassifierTypeCodeID
+  , @ClassifierCodeID
+  , @ClassifierRelationshipTypeID
+  , @RelatedClassifierTypeCodeID
+  , @RelatedClassifierCodeID
+  , 1
+  )
+;
