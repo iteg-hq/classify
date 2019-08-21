e5ce26bd12667ea82ac6ea0188ff261f347ffb88
Frederik Siegumfeldt <fsi@iteg.dk>
2019-08-21 14:08:06 +0200

    Moved postdeploy script and prevented reinsert of system data rows.

---
+++ Classify/Script.PostDeployment.sql
@@ -0,0 +1,9 @@
+﻿-- Insert system data rows
+SET IDENTITY_INSERT internal.Code ON
+IF NOT EXISTS (SELECT 1 FROM internal.Code WHERE CodeID = -1 )
+  INSERT INTO internal.Code ( CodeID, CodeValue ) VALUES ( -1, '' )
+IF NOT EXISTS (SELECT 1 FROM internal.Code WHERE CodeID = 0 )
+  INSERT INTO internal.Code ( CodeID, CodeValue ) VALUES ( 0, 'ClassifierRelationshipType' )
+SET IDENTITY_INSERT internal.Code OFF
+
+EXEC SaveClassifierType 'ClassifierRelationshipType', 'Classifier Relationship Type', 'The type of classifiers that classify classifier relationships';
