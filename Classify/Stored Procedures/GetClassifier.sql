232a3dcd2e2d8dc4061fe9307d8e28986b9bcf84
Frederik Siegumfeldt <fsi@iteg.dk>
2019-09-17 21:39:36 +0200

    Added GetClassifier to get a single classifier

---
+++ Classify/Stored Procedures/GetClassifier.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE dbo.GetClassifier
+    @ClassifierTypeCode NVARCHAR(500)
+  , @ClassifierCode NVARCHAR(500)
+AS
+SET NOCOUNT, XACT_ABORT ON;
+
+SELECT
+    ClassifierTypeCode
+  , ClassifierCode
+  , ClassifierName
+  , ClassifierDescription
+  , UpdatedBy
+  , UpdatedOn
+FROM dbo.Classifier
+WHERE ClassifierTypeCode = @ClassifierTypeCode
+  AND ClassifierCode = @ClassifierCode
+;
