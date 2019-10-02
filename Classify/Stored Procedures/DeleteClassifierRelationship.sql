07876732577fa477abb5d81daa5a5d2f091907c9
Frederik Siegumfeldt <fsi@iteg.dk>
2019-10-02 09:40:49 +0200

    Naming fixed

--- Classify/Stored Procedures/DeleteClassifierRelationship.sql
+++ Classify/Stored Procedures/DeleteClassifierRelationship.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE dbo.DeleteClassifierRelationship
     @ClassifierTypeCode NVARCHAR(200)
   , @ClassifierCode NVARCHAR(200)
-  , @ClassifierRelationshipType NVARCHAR(200)
+  , @ClassifierRelationshipTypeCode NVARCHAR(200)
   , @RelatedClassifierTypeCode NVARCHAR(200)
   , @RelatedClassifierCode NVARCHAR(200)
 AS
@@ -9,15 +9,15 @@ SET NOCOUNT, XACT_ABORT ON;
 
 DECLARE @ClassifierTypeCodeID INT;
 DECLARE @ClassifierCodeID INT;
-DECLARE @ClassifierRelationshipTypeID INT;
+DECLARE @ClassifierRelationshipTypeCodeID INT;
 DECLARE @RelatedClassifierTypeCodeID INT;
 DECLARE @RelatedClassifierCodeID INT;
 
-EXEC internal.GetCodeID @ClassifierTypeCode,         @ClassifierTypeCodeID OUTPUT;
-EXEC internal.GetCodeID @ClassifierCode,             @ClassifierCodeID OUTPUT;
-EXEC internal.GetCodeID @ClassifierRelationshipType, @ClassifierRelationshipTypeID OUTPUT;
-EXEC internal.GetCodeID @RelatedClassifierTypeCode,  @RelatedClassifierTypeCodeID OUTPUT;
-EXEC internal.GetCodeID @RelatedClassifierCode,      @RelatedClassifierCodeID OUTPUT;
+EXEC internal.GetCodeID @ClassifierTypeCode,             @ClassifierTypeCodeID OUTPUT;
+EXEC internal.GetCodeID @ClassifierCode,                 @ClassifierCodeID OUTPUT;
+EXEC internal.GetCodeID @ClassifierRelationshipTypeCode, @ClassifierRelationshipTypeCodeID OUTPUT;
+EXEC internal.GetCodeID @RelatedClassifierTypeCode,      @RelatedClassifierTypeCodeID OUTPUT;
+EXEC internal.GetCodeID @RelatedClassifierCode,          @RelatedClassifierCodeID OUTPUT;
 
 INSERT INTO internal.ClassifierRelationship (
     ClassifierTypeCodeID
@@ -30,7 +30,7 @@ INSERT INTO internal.ClassifierRelationship (
 VALUES (
     @ClassifierTypeCodeID
   , @ClassifierCodeID
-  , @ClassifierRelationshipTypeID
+  , @ClassifierRelationshipTypeCodeID
   , @RelatedClassifierTypeCodeID
   , @RelatedClassifierCodeID
   , 1
